7db2b89c26857ed20b432cd7ffe4e031fc25fe8a
Zach Nielsen <zachary.nielsen@tektronix.com>
2019-09-21 13:12:11 -0700

    Added user input test.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 extern crate serde;
 extern crate serde_pickle;
+extern crate chrono;
 
-use std::{fs::File, io::prelude::*};
+use std::{fs::File};
+use std::io::{prelude::*, stdin, stdout, Write}
 use serde::{Serialize, Deserialize};
-// use serde_json::{Result};
 
 const PICKLE_FILE : &str = "json.pkl";
 
@@ -26,22 +27,52 @@ struct Budget {
     field: Field,
     cat: Category,
 }
-
-fn main() {
-    println!("Hello, world!");
+#[derive(Serialize, Deserialize, Debug)]
+struct Transactions {
+    id    : u128,
+    date  : chrono::Date,
+    payee : String,
+    category: String,
+    outflow: Option<i32>,
+    inflow: Option<i32>,
+    cleared: TransactionStatus,
+}
+enum TransactionStatus {
+    CreditUncleared;
+    CreditCleared;
+    DirectBank;
 }
 
+fn main() {
+    println!("Rust JSON interface");
+    let mut input = String::new();
+    while input != "exit" {
+        input = get_input();
 
-fn pickle_json(b: &Budget, file: &str) -> serde_pickle::Result<()> {
+        println!("Got : {}", input);
+    }
+    println!("Exiting");
+}
+
+fn get_input() -> String {
+    print("> ");
+    stdin()
+        .read_line(&mut s)
+        .expect("Error while receiving input")
+        .trim()
+        .to_owned()
+}
+
+fn pickle_json(b: &JsonTop, file: &str) -> serde_pickle::Result<()> {
     let ser = serde_pickle::to_vec(&b, true)?;
     let mut fp = File::open(file)?;
     fp.write(&ser)?;
     Ok(())
 }
-fn depickle_json(file: &str) -> serde_pickle::Result<Budget> {
+fn depickle_json(file: &str) -> serde_pickle::Result<JsonTop> {
     let mut fp = File::open(file)?;
     let mut ser = String::new();
     fp.read_to_string(&mut ser)?;
-    let b: Budget = serde_json::from_str(ser.as_str()).unwrap();
+    let b: JsonTop = serde_json::from_str(ser.as_str()).unwrap();
     Ok(b)
 }
